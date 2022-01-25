bce85def2ab7262eb94cdb5df064c6b299e642a0
CMBill <callmebill11@outlook.com>
2022-01-25 13:49:52 +0800

    20220125-1

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import pandas as pd
-
-headers = {
-    'Accept': 'application/json',
-    'x-api-key': '$2a$10$LhtoxmW9RQT3gXvSQDgXZuTRAwXKzZMlzcY0CKj5LNVC7o61en7ga'
-}
+import json
 
 
 # 获取某一mod所有的文件列表并以数据框返回
 def get_files(modid):
+    headers = {
+        'Accept': 'application/json',
+        'x-api-key': '$2a$10$LhtoxmW9RQT3gXvSQDgXZuTRAwXKzZMlzcY0CKj5LNVC7o61en7ga'
+    }
     url = 'https://api.curseforge.com/v1/mods/' + modid + '/files'
     r = requests.get(url, headers=headers)
     r_meta = r.json()
@@ -23,6 +23,8 @@ def get_files(modid):
                 modloader = 'Fabric'
             elif gameVersions[j] == 'Forge':
                 modloader = 'Forge'
+            elif gameVersions[j] == 'Rift':
+                modloader = 'Rift'
             else:
                 MCVers.append(gameVersions[j])
 
@@ -37,6 +39,30 @@ def get_files(modid):
 
 
 # 根据需求从数据框中返回下载Url
-def get_url(files_meta):
-
+def get_url(files_meta, loader, gver):
+    meta_select = files_meta.loc[files_meta['modloader'] == loader, ]
+    meta_select = meta_select.loc[meta_select['MCVersions'] == gver, ]
+    download_url = meta_select.loc[meta_select['fileDate'] == meta_select.loc[:, 'fileDate'].max(), 'downloadUrl'].at[0]
     return download_url
+
+
+def main():
+    with open('./config.json') as config_meta:
+        config = json.load(config_meta)
+
+    gamever = config['MC_Version']
+    modloader = config['Mod_loader']
+    modids = config['ModIDs']
+    d_urls = []
+
+    for i in range(len(modids)):
+        modid = modids[i]
+        files_meta = get_files(modid)
+        d_url = get_url(files_meta, modloader, gamever)
+        d_urls.append(d_url)
+    downloadUrls = json.dumps(d_urls)
+    return downloadUrls
+
+
+if __name__ == '__main__':
+    main()
